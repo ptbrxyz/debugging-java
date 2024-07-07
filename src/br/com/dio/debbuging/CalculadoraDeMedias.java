cf892d662e7d3cec01e89eedb143c24bf3850f9a
Pytanga BR <ptbr.xyz@gmail.com>
2024-07-07 12:19:54 -0300

    Debugging com IntelliJ.

--- src/br/com/dio/debbuging/CalculadoraDeMedias.java
+++ src/br/com/dio/debbuging/CalculadoraDeMedias.java
@@ -7,17 +7,21 @@ public class CalculadoraDeMedias {
         Scanner scan = new Scanner(System.in);
         String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
 
+        //int media = calculaMediaDaTurma(alunos, scan);
         double media = calculaMediaDaTurma(alunos, scan);
 
         System.out.printf("Média da turma %.1f", media);
+        //System.out.printf("Média da turma %d", media);
     }
-
     public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+    //public static int calculaMediaDaTurma(String[] alunos, Scanner scanner) {
 
         double soma = 0;
+        //int soma = 0;
         for(String aluno : alunos) {
             System.out.printf("Nota do aluno %s: ", aluno);
             double nota = scanner.nextDouble();
+            //int nota = scanner.nextInt();
             soma += nota;
         }
 
